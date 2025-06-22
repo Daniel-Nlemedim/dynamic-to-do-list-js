fb9fbdb7ad590390590677a7f987fcc4ee3da638
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-06-22 17:35:37 +0100

    Add initial files for To-Do List application with basic functionality

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const addButton = document.getElementById('add-task-btn');
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+
+    function addTask() {
+        const taskText = taskInput.value.trim();
+        if(taskText === '') {
+            alert('Please enter a task.');
+            return;
+        }
+        else{
+            const removeButton = document.createElement('button');
+            const newTask = document.createElement('li');
+            newTask.textContent = taskText;
+            removeButton.textContent = 'remove';
+            removeButton.className = 'remove-btn';
+            removeButton.onclick = function() {
+            taskList.removeChild(newTask);
+            };
+
+            newTask.appendChild(removeButton);
+            taskList.appendChild(newTask);
+
+            taskInput.value = '';
+        }
+
+    }
+
+    addButton.addEventListener('click', addTask);
+    taskInput.addEventListener('keypress', function(event){
+        if(event.key === 'Enter'){
+            addTask();
+        }
+    })
+})
